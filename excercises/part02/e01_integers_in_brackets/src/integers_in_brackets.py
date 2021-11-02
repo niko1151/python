0ef24c912896de1ef660b896698ab81048cf83a7
Nikolai <nsflodin@gmail.com>
2021-11-02 12:10:13 +0100

    part2 1, and 17 done working in 2

--- excercises/part02/e01_integers_in_brackets/src/integers_in_brackets.py
+++ excercises/part02/e01_integers_in_brackets/src/integers_in_brackets.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+import re
 
 def integers_in_brackets(s):
-    return []
+    r= []
+    patt = re.compile(r'\[\s*([+-]?\d+)\s*\]')
+    for i in patt.findall(s):
+        r.append(int(i))
+    return r
 
 def main():
-    pass
+    integers_in_brackets("  afd [asd] [12 ] [a34]  [ -43 ]tt [+12]xxx")
 
 if __name__ == "__main__":
     main()
--- excercises/part02/e02_file_listing/src/file_listing.py
+++ excercises/part02/e02_file_listing/src/file_listing.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
 import re
+import datetime
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
+    rows = filename.split("\n")
+    new_rows = []
+
+    for row in rows:
+        row_data = row.split()
+        mode = row_data[0]
+        hardlink_count = row_data[1]
+        user = row_data[2]
+        group = row_data[3]
+        size = row_data[4]
+        month = row_data[5]
+        day = row_data[6]
+        hour = row_data[7]
+        filename = row_data[8]
+        # Also doable in a single line in Python 3 with:
+        # mode, hardlink_count, user, group, size, month, day, time, *filename = row.split()
+        new_rows.append((filename, size, month, day, hour))
+        # You can use something similar if you need to transform the string date
+        # into a date object:
+        #new_rows.append((filename, size, datetime.datetime.strptime("{}-{} {}".format(month, day, hour), '%b-%d %H:%M')))
+
+    return new_rows
 
 def main():
-    pass
+    filepath = "C:\Users\Tec\Python-og-DataAnalyse-1\excercises\part02\e02_file_listing\src\listing.txt"
+    file_listing(filepath)
 
 if __name__ == "__main__":
     main()
