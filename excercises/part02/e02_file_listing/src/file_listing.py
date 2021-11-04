d43b21f6ccf9540ad8dcde29f925d64141f8597e
NikolaiInk <59829160+NikolaiInk@users.noreply.github.com>
2021-11-04 19:43:54 +0100

    finished until 12

--- excercises/part02/e02_file_listing/src/file_listing.py
+++ excercises/part02/e02_file_listing/src/file_listing.py
@@ -1,36 +1,19 @@
 #!/usr/bin/env python3
 
 import re
-import datetime
-
 
 def file_listing(filename="src/listing.txt"):
-    rows = filename.split("\n")
-    new_rows = []
+    res = []
 
-    for row in rows:
-        row_data = row.split()
-        mode = row_data[0]
-        hardlink_count = row_data[1]
-        user = row_data[2]
-        group = row_data[3]
-        size = row_data[4]
-        month = row_data[5]
-        day = row_data[6]
-        hour = row_data[7]
-        filename = row_data[8]
-        # Also doable in a single line in Python 3 with:
-        # mode, hardlink_count, user, group, size, month, day, time, *filename = row.split()
-        new_rows.append((filename, size, month, day, hour))
-        # You can use something similar if you need to transform the string date
-        # into a date object:
-        #new_rows.append((filename, size, datetime.datetime.strptime("{}-{} {}".format(month, day, hour), '%b-%d %H:%M')))
-
-    return new_rows
+    with open(filename, 'r') as f:
+        for l in f:
+            temp = re.findall(r'\S+\s\d\s\w+\s\S+\s+(\d+)\s([A-Za-z]{3})\s+(\d{1,})\s(\d{2}):(\d{2})\s(.+)', l)
+            temp = (int(temp[0][0]), temp[0][1], int(temp[0][2]), int(temp[0][3]), int(temp[0][4]), temp[0][5])
+            res.append(temp)
+    return res
 
 def main():
-    filepath = "C:\Users\Tec\Python-og-DataAnalyse-1\excercises\part02\e02_file_listing\src\listing.txt"
-    file_listing(filepath)
+    print(file_listing())
 
 if __name__ == "__main__":
     main()
--- excercises/part02/e03_red_green_blue/src/red_green_blue.py
+++ excercises/part02/e03_red_green_blue/src/red_green_blue.py
@@ -3,11 +3,16 @@
 import re
 
 def red_green_blue(filename="src/rgb.txt"):
-    return []
-
+      with open('src/rgb.txt') as f:
+        for line in f:
+            line = f.read()
+            regex = re.findall(r"[ \t]*(\d+)[ \t]*(\d+)[ \t]*(\d+)[ \t]*(.*)", line)
+            string_list = list(map('\t'.join, regex))
+            print(string_list)
+        return string_list
 
 def main():
-    pass
+    print(red_green_blue())
 
 if __name__ == "__main__":
     main()
